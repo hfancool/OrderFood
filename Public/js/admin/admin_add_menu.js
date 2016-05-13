0c838925cecaccde81040bb23e6e405d4bf9c382
hfan <hfan@3ti.us>
2016-05-13 08:51:40 +0800

    菜单图片上传样式修改

--- Public/js/admin/admin_add_menu.js
+++ Public/js/admin/admin_add_menu.js
@@ -5,4 +5,18 @@ $(function(){
     //    window.history.back();
     //
     //});
+    /*文件上传框样式*/
+    $(".file").on("change","input[type='file']",function(){
+        var filePath=$(this).val();
+        if(filePath.indexOf("jpg")!=-1 || filePath.indexOf("png")!=-1){
+            $(".fileerrorTip").html("").hide();
+            var arr=filePath.split('\\');
+            var fileName=arr[arr.length-1];
+            $(".showFileName").html(fileName);
+        }else{
+            $(".showFileName").html("");
+            $(".fileerrorTip").html("您未上传文件，或者您上传文件类型有误！").show();
+            return false
+        }
+    })
 });
